787f4e95d04b6131ce00c9b735e2e4608a2d3300
Christopher Thai <christopherthai.nyli@gmail.com>
2024-04-06 19:22:50 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "red", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark blue", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 19, "M", "Pink", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark yellow", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Nice", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "Happy", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "Purple", 0);
